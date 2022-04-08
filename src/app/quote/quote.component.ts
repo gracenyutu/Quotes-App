edcb6c77374be88e809bc7d0cf84fe09ebcd0d21
Grace Nyutu <graceynyutu@gmail.com>
2022-04-08 14:04:25 +0300

    create quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { Quotes } from './quotes';
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class AppComponent {
+export class QuoteComponent implements OnInit {
+
   quotes:Quotes[] = [
     {id:1, quote: 'When something is important enough, you do it even if the odds are not in your favor', author: 'Elon Musk'},
     {id:2, quote: 'Its good to learn from mistakes, not just your own', author: 'Elon Musk'},
@@ -17,4 +18,10 @@ export class AppComponent {
     {id:7, quote: 'Goodness is the only investment that never fails', author: 'Henry David Thoreau'},
     {id:8, quote: 'Integrity reveals beauty', author: 'Thomas Leonard'}
   ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
